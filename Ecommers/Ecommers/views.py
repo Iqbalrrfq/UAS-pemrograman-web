83cf6c0e67c47ab5ede34542f0cd64b65b9d5fb2
Iqbalur Rofiq <ieqbalpunxa05@gmail.com>
2023-12-28 18:26:45 +0700

    push ke-6

--- Ecommers/Ecommers/views.py
+++ Ecommers/Ecommers/views.py
@@ -3,6 +3,5 @@ from django.shortcuts import render
 def index(request):
     context = {
         'judul':'Ecommers',
-        'subjudul':'Selamat Datang',
     }
     return render(request,'index.html',context)
